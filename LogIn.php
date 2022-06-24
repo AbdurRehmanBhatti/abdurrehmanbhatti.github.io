12ef2313f3020f254c161696e1ea45dfa0fc0683
AbdurRehmanBhatti <104156517+AbdurRehmanBhatti@users.noreply.github.com>
2022-06-24 12:37:15 +0500

    Update and rename LogIn.html to LogIn.php

--- LogIn.php
+++ LogIn.php
@@ -57,4 +57,43 @@
       </div>
     </form>
   </body>
+  if(isset($_POST['login'])){
+		
+		$email= $_POST['email'];
+		$pass=$_POST['pass'];
+		
+		$check = "select*from user where email='$email' AND password ='$pass'";
+		$check_work= mysqli_query($con,$check);
+		
+		if($check_work){
+			if(mysqli_num_rows($check_work) > 0 ){
+				
+				echo"
+				<script>
+				alert('You are Successfully  Logged in');
+				window.location.href='login.php';
+				</script>
+				";
+				
+			}else{
+				
+				echo"
+				<script>
+				alert('Password or Email not Found ');
+				window.location.href('signup.php');
+				</script>
+				";
+			}
+		}else{
+				echo"
+				<script>
+				alert('Database Error  ');
+				window.location.href('signup.php');
+				</script>
+				";
+		}	
+	}else{
+  
+	}
+?>
 </html>
